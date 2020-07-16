3ee600007eb1c01a7739b146620e71f82024368c
Derek <derektradd@gmail.com>
2020-07-15 20:45:42 -0400

    Organizing server.js file

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
+//Importing external packages required for the project
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
-}
+};
 
 const express = require('express');
-const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const indexRouter = require('./routes/index')
-const authorRouter = require('./routes/authors')
-
+// Setting up the app with express and set server settings
+const app = express();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.set('layout', 'layouts/layout')
+app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({limit: '10mb', extended:false}))
+app.use(bodyParser.urlencoded({limit: '10mb', extended:false}));
 
-const mongoose = require('mongoose');
+//Setting up the mongo database
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+});
 const db = mongoose.connection;
 db.on('error', error => console.log(error));
-db.once('open', () => console.log('Connected to Mongoose'))
+db.once('open', () => console.log('Connected to Mongoose'));
 
-
+//Setting up the server to listen on port 8000 or the environment variable
 app.listen(process.env.PORT || 8000, () => {
     console.log('Listening on port 8000.')
 });
 
-app.use('/', indexRouter)
-app.use('/authors', authorRouter)
+// Route Definitions imported from /routes folder
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+
+app.use('/', indexRouter);
+app.use('/authors', authorRouter);
