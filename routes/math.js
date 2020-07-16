b49a0cfea210c78dbf6590d9d5d0696713672c9c
Derek <derektradd@gmail.com>
2020-07-16 12:04:23 -0400

    folder refactor

--- routes/math.js
+++ routes/math.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mathApi = require('../src/js/mathApi')
+const mathApi = require('../models/mathApi')
 
 // Get math facts route
 router.get('/', async (req, res) => {
