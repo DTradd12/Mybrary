3072eaa7790759b5331df302bb1b33dfb0900f57
Derek <derektradd@gmail.com>
2020-07-16 13:36:23 -0400

    refactoring

--- routes/math.js
+++ routes/math.js
@@ -4,14 +4,26 @@ const mathApi = require('../models/mathApi')
 
 // Get math facts route
 router.get('/', async (req, res) => {
-    let fact = await mathApi(req.query.number).then(result => {
-        return result
-    })
-    res.render('math/index', {
-        fact: fact,
-        number: req.query.number
-    })
-})
-
+    console.log(req.query.number);
+    if (req.query.number){
+        try{
+            let fact = await mathApi(req.query.number).then(result => {
+                return result
+            })
+            res.render('math/index', {
+                data: "true",
+                number: req.query.number,
+                fact: fact
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    } else {
+        res.render('math/index', {
+            data: "false"
+        })
+        }
+    }
+)
 
 module.exports = router
